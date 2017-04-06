64f8c5249c23251b3063adf3c133488cef920ce6
alipov <tech.alexl@gmail.com>
2017-04-06 15:45:19 +0300

    Adding crashlytics.

---
+++ app/src/main/java/info/osom/droidconcd/MyApplication.java
@@ -0,0 +1,14 @@
+package info.osom.droidconcd;
+
+import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
+}
